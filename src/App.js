361022515b50d0fc127a18c8413b9fa1d61d452c
unknown <saiponekhaaaung@gmail.com>
2022-07-26 22:12:20 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import TheBoy from "./Theboy";
+import "./styles.css";
 
 function App() {
+  const [action, setAction] = useState("idle");
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas camera={{ position: [0, 1, 2] }}>
+
+        <ambientLight />
+        <Suspense fallback={null}>
+          <TheBoy action={action} />
+        </Suspense>
+        <OrbitControls traget={[0, 1, 0]} autoRotate />
+
+      </Canvas>
+      <div className="controls">
+        <button onClick={() => setAction("dance")}>Dance</button>
+        <button onClick={() => setAction("dance2")}>Dance 2</button>
+        <button onClick={() => setAction("idle")}>idle</button>
+      </div>
+
+
+
+
+    </>
   );
 }
 
